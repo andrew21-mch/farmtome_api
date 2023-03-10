258c337dd9d57382762dc366d1b9deed2bec1ae0
andrew21-mch <nfonandrew73@gmail.com>
2023-03-10 16:03:35 +0100

    initialise app

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,16 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+Route::get('/', function () {
+    return response()->json([
+        'message' => 'Welcome to the API',
+        'version' => '1.0.0',
+        'description' => 'This is an api to link consumer, farmers, and suppliers together'.
+                            'to make the process of buying and selling of agricultural products easier'.
+                            'and more efficient.'.
+                            'The api is built using laravel and is hosted on heroku and can be accessed at'.
+                            'https://agroconnect-api.herokuapp.com/api/v1',
+    ]);
+});
