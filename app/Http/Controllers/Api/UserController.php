5d13fb695aa0f1a331c22edabec53fd4c854252b
andrew21-mch <nfonandrew73@gmail.com>
2023-03-17 01:46:21 +0100

    fix psr-4

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -1,9 +1,10 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Api;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Routing\Controller;
 
 class UserController extends Controller
 {
